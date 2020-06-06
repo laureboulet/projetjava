4bf806365362073232ba682e5e504210fc047d67
laureboulet <49633566+laureboulet@users.noreply.github.com>
2020-06-06 22:06:30 +0200

    Add files via upload

--- Controleur_edt.java
+++ Controleur_edt.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import modele.Cours;
 import modele.CoursDAO;
+import modele.Enseignant;
+import modele.EnseignantDAO;
 import modele.Etudiant;
 import modele.EtudiantDAO;
 import modele.Groupe;
@@ -86,7 +88,6 @@ public class Controleur_edt {
                 }else {
                     etat = "annulé";
                 }
-                System.out.println(etat);
                 date [0]=etat;
                 //casee 2
                 Cours cours = new Cours();
@@ -97,7 +98,6 @@ public class Controleur_edt {
                 Type_coursDAO typed = new Type_coursDAO();
                 type = typed.find(seance.getId_type());
                 String typecours = type.getNom();
-                System.out.println(nomcours+","+typecours);
                 date[1]=nomcours+","+typecours;
                 //case 3
                 Seance_enseignants sens = new Seance_enseignants();
@@ -110,7 +110,6 @@ public class Controleur_edt {
                 Groupe gr = new Groupe();
                 GroupeDAO grd = new GroupeDAO();
                 gr = grd.find(et.getId_groupe());
-                System.out.println(enseignant+","+gr.getNom());
                 date[2]=enseignant+","+gr.getNom();
                 //case 4 
                 Salle s = new Salle();
@@ -125,9 +124,7 @@ public class Controleur_edt {
                 Site si = new Site();
                 SiteDAO sid = new SiteDAO();
                 si=sid.find(s.getId_site());
-                String site = si.getNom();
-                System.out.println(salle+","+site);
-                System.out.println(seance.getId()+","+sesa.getId_salle()+","+s.getNom());       
+                String site = si.getNom();     
                 date[3] = salle+","+site;
                 
                 if(seance.getHeure_debut() ==800) {
@@ -148,11 +145,9 @@ public class Controleur_edt {
                     donnees[7][2]="<html>" + date[0]  +"<br>" + date[1] +"<br>" +date[2] +  "<br>" +date[3] +"</html>";
                 }
                
-               System.out.println("On passe au cours suivant");
             }
             
-      //<html>" + date[0][0]  +"<br>" + date[1][0] + date[1][1] +"<br>" +date[2][0] +date[2][1] +"</html>
-            //Object[][] date = {{"etat"},{"nomcours","typecours"},{"enseignant","groupe"},{"salle","site"}};
+    
           
 
 
@@ -164,8 +159,142 @@ public class Controleur_edt {
             Edt table = new Edt(mod);
             table.setVisible(true);
         }
-        else{
-            System.out.println("Tu n'es pas un etudiant!");
+        else if(droit==3){
+            Enseignant ens = new Enseignant(ut.getId());
+            EnseignantDAO ensd = new EnseignantDAO();
+            Seance_enseignants se = new Seance_enseignants();
+            List<Seance_enseignants> obj = new ArrayList<>();
+            Seance_enseignantsDAO sed = new Seance_enseignantsDAO();
+            obj=sed.findList(ens.getId_utilisateur());
+            Seance seance = new Seance();
+            SeanceDAO seanced = new SeanceDAO();
+            
+            int idSeance=0;
+            
+              Object[][] donnees = {
+                        {"8:00-9:30","","","","","",""},
+                        {"9:30-11:00","","","","","",""},
+                        {"11:00-12:30","","","","","",""},
+                        {"12:30-14:00","","","","","",""},
+                        {"14:00-15:30","","","","","",""},
+                        {"15:30-17:00","","","","","",""},
+                        {"17:00-18:30","","","","","",""},
+                        {"18:30-20:00","","","","","",""},
+                        {"20:00-21:30","","","","","",""}};
+              
+            
+            for(Seance_enseignants it : obj){
+                Object[] date = new String[4];
+                //case 1
+                idSeance = it.getId_seance();
+                seance= seanced.find(idSeance);
+                String etat;
+                if(seance.getEtat()==1){
+                    etat = "En cours de validation";
+                }else if(seance.getEtat()==2){
+                    etat = "validé";
+                }else {
+                    etat = "annulé";
+                }
+                date [0]=etat;
+                 //casee 2
+                Cours cours = new Cours();
+                CoursDAO coursd = new CoursDAO();
+                cours= coursd.find(seance.getId_cours());
+                String nomcours = cours.getNom();
+                Type_cours type = new Type_cours();
+                Type_coursDAO typed = new Type_coursDAO();
+                type = typed.find(seance.getId_type());
+                String typecours = type.getNom();
+                date[1]=nomcours+","+typecours;
+                //case 3
+                Seance_groupes sg = new Seance_groupes();
+                Seance_groupesDAO sd = new Seance_groupesDAO();
+                sg = sd.findS(seance.getId());
+                String enseignant = ut.getNom();
+                Groupe gr = new Groupe();
+                GroupeDAO grd = new GroupeDAO();
+                gr = grd.find(sg.getId_groupe());
+                date[2]=enseignant+","+gr.getNom();
+                //case 4 
+                Salle s = new Salle();
+                SalleDAO sad = new SalleDAO();
+                Seance_salles sesa = new Seance_salles();
+                Seance_sallesDAO sesad = new Seance_sallesDAO();
+                //on cherche l'id de la salle correspondant a cette seance
+                sesa = sesad.find(seance.getId());
+                //on cherche la salle correspondant à cet id 
+                s=sad.find(sesa.getId_salle());
+                String salle = s.getNom();
+                Site si = new Site();
+                SiteDAO sid = new SiteDAO();
+                si=sid.find(s.getId_site());
+                String site = si.getNom();     
+                date[3] = salle+","+site;
+                
+                if(seance.getHeure_debut() ==800) {
+                    donnees[0][2]="<html>" + date[0] +"<br>" + date[1]  +"<br>" +date[2] + "<br>" +date[3] +"</html>";
+                } else if(seance.getHeure_debut() ==930) {
+                    donnees[1][2]="<html>" + date[0]  +"<br>" + date[1]  +"<br>" +date[2] +  "<br>" +date[3] +"</html>";
+                } else if (seance.getHeure_debut()==1100){
+                    donnees[2][2]="<html>" + date[0] +"<br>" + date[1] +"<br>" +date[2] +  "<br>" +date[3] +"</html>";
+                } else if (seance.getHeure_debut()==1230){
+                    donnees[3][2]="<html>" + date[0]  +"<br>" + date[1] +"<br>" +date[2] +  "<br>" +date[3] +"</html>";
+                } else if (seance.getHeure_debut()==1400){
+                    donnees[4][2]="<html>" + date[0]  +"<br>" + date[1] +"<br>" +date[2] +  "<br>" +date[3] +"</html>";
+                } else if (seance.getHeure_debut()==1530){
+                    donnees[5][2]="<html>" + date[0]  +"<br>" + date[1] +"<br>" +date[2] +  "<br>" +date[3] +"</html>";
+                } else if (seance.getHeure_debut()==1700){
+                    donnees[6][2]="<html>" + date[0] +"<br>" + date[1] +"<br>" +date[2]+  "<br>" +date[3] +"</html>";
+                } else if (seance.getHeure_debut()==1830){
+                    donnees[7][2]="<html>" + date[0]  +"<br>" + date[1] +"<br>" +date[2] +  "<br>" +date[3] +"</html>";
+                }
+            }
+            
+            
+             //date,salle,site,nom et type de cours,enseignant,groupe
+            String[] entetes = {" ","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"};
+            Modele mod = new Modele(donnees,entetes);
+
+            Edt table = new Edt(mod);
+            table.setVisible(true);
+            
+        }else if(droit == 2){
+            Object[][] donnees = {
+                        {"8:00-9:30","","","","","",""},
+                        {"9:30-11:00","","","","","",""},
+                        {"11:00-12:30","","","","","",""},
+                        {"12:30-14:00","","","","","",""},
+                        {"14:00-15:30","","","","","",""},
+                        {"15:30-17:00","","","","","",""},
+                        {"17:00-18:30","","","","","",""},
+                        {"18:30-20:00","","","","","",""},
+                        {"20:00-21:30","","","","","",""}};
+            
+            String[] entetes = {" ","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"};
+            Modele mod = new Modele(donnees,entetes);
+
+            Edt table = new Edt(mod);
+            table.setVisible(true);
+            
+        } else if(droit==1){
+            Object[][] donnees = {
+                        {"8:00-9:30","","","","","",""},
+                        {"9:30-11:00","","","","","",""},
+                        {"11:00-12:30","","","","","",""},
+                        {"12:30-14:00","","","","","",""},
+                        {"14:00-15:30","","","","","",""},
+                        {"15:30-17:00","","","","","",""},
+                        {"17:00-18:30","","","","","",""},
+                        {"18:30-20:00","","","","","",""},
+                        {"20:00-21:30","","","","","",""}};
+            
+            String[] entetes = {" ","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"};
+            Modele mod = new Modele(donnees,entetes);
+
+            Edt table = new Edt(mod);
+            table.setVisible(true);
+            
         }
     }
 }
