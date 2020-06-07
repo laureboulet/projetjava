6f531a83c840c11a587cafa24e04ef17211e3245
bourgat <47519287+bourgat@users.noreply.github.com>
2020-06-07 19:34:21 +0200

    Add files via upload

--- Edt.java
+++ Edt.java
@@ -5,6 +5,8 @@
  */
 package vue;
 
+import controleur.Controleur_edt;
+import controleur.Controleur_maj;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -17,21 +19,22 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.List;
 import javax.swing.*;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
+import modele.Utilisateur;
 
 /**
  *
  * @author laure et clemence
  */
 public class Edt extends JFrame{
-    public JTable tableau/*, tableau2*/;
+    public JTable tableau;
     public JPanel edt;
     public Recapitulatif recap;
     public JTabbedPane emploi;
-    public JFrame ajouter = new JFrame();
-    public JComboBox filtre;
+    public JFrame ajouter = new JFrame(); 
     public JMenuBar menu = new JMenuBar();
     public JMenu cours = new JMenu("Cours");
     public JMenu groupes = new JMenu("Groupes");
@@ -40,20 +43,19 @@ public class Edt extends JFrame{
     public JMenuItem cours1 = new JMenuItem("Modifier le type du cours");
     public JMenuItem cours2 = new JMenuItem("Modifier le nom du cours");
     public JMenuItem cours3 = new JMenuItem("Déplacer la séance");
-    public JMenuItem cours4 = new JMenuItem("Ajouter une séance");
-    
+    public JMenuItem cours4 = new JMenuItem("Ajouter une séance");  
     public JMenuItem groupes2 = new JMenuItem("Ajouter à une séance");
     public JMenuItem groupes3 = new JMenuItem("Enlever à une séance");
     public JMenuItem enseignants2 = new JMenuItem("Ajouter à une séance");
     public JMenuItem enseignants3 = new JMenuItem("Enlever à une séance");
     public JMenuItem salles1 = new JMenuItem("Salles disponibles");
     public JMenuItem salles2 = new JMenuItem("Affecter à une séance");
-    
-    
+    public JComboBox ens,gr,cou;
+    public JButton ok;
     
     public Edt(){
-        
     }
+
     public Edt(Modele mod){
         this.setTitle("Emploi du temps");
         this.setBounds(200, 0, 1000, 800);
@@ -65,25 +67,10 @@ public class Edt extends JFrame{
         edt.setBackground(Color.red);
         recap.setLayout(new GridLayout());
         
-        //creation du filtre d'affichage de l'emploi du temps pour admin et referent pedagogique
-        filtre = new JComboBox();
-        filtre.addItem("Cours");
-        filtre.addItem("Groupes");
-        filtre.addItem("Enseignants");
-        filtre.setPreferredSize(new Dimension(50, 25));
-        
         //creation des menus 
         this.cours.add(cours1);
         this.cours.add(cours2);
         this.cours.add(cours3);
-        
-        /*cours4.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent arg0) {
-                Maj nouv = null;
-                nouv = new Maj(ajouter, "Ajouter une séance", true);
-                nouv.setVisible(true);
-            }         
-        });  */
         this.cours.add(cours4);
         this.groupes.add(groupes2);
         this.groupes.add(groupes3);
@@ -91,50 +78,15 @@ public class Edt extends JFrame{
         this.enseignants.add(enseignants3);
         this.salles.add(salles1);
         this.salles.add(salles2);
-        /*this.menu.add(cours);
-        this.menu.add(groupes);
-        this.menu.add(enseignants);
-        this.menu.add(salles);*/
-        this.menu.add(filtre);
+        ens = new JComboBox();
+        gr = new JComboBox();
+        cou = new JComboBox();
+        ok = new JButton("Ok");
         
         //creation des onglets
         emploi = new JTabbedPane();
         emploi.addTab("Emploi du temps", edt); //emploi du temps
         emploi.addTab("Récapitulatifs des cours", recap); //recap des cours
-        
-        //creation du tableau d'affichage des récapitulatifs des cours
-        /*Object[][] data = { 
-                    {"","","","",""},
-                    {"","","","",""},
-                    {"","","","",""},
-                    {"","","","",""},
-                    {"","","","",""},
-                    {"","","","",""},
-                    {"","","","",""},
-                    {"","","","",""},
-                    {"","","","",""},
-                    {"","","","",""}};
-        //ou alors juste List<Seance> seances = new ArrayList<Seance>();
-        String[] header = {"Matières","Première séance","Dernière séance","Durée","Nb."};
-        Modele model2 = new Modele(data,header);
-        tableau2 = new JTable(model2);*/
-        
-        //creation du tableau d'affichage de l'emploi du temps
-        /*Object[][] date = {{"etat"},{"nomcours","typecours"},{"enseignant","groupe"},{"salle","site"}};
-        Object[][] donnees = {
-                    {"8:00-9:30","","","","","",""},
-                    {"9:30-11:00","","","","","",""},
-                    {"11:00-12:30","","","","","",""},
-                    {"12:30-14:00","","","","","",""},
-                    {"14:00-15:30","","","","","",""},
-                    {"15:30-17:00","","","","","",""},
-                    {"17:00-18:30","","","","","",""},
-                    {"18:30-20:00","","","","","",""},
-                    {"20:00-21:30","","","","","",""}};
-        
-        //date,salle,site,nom et type de cours,enseignant,groupe
-        String[] entetes = {" ","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"};
-        Modele model = new Modele(donnees,entetes);*/
         tableau = new JTable(mod);
         tableau.setRowHeight(80);
         tableau.setGridColor(Color.gray);
@@ -159,11 +111,11 @@ public class Edt extends JFrame{
                 if (rowindex < 0 || colindex < 1) //pour ne pas avoir de popup a la colonne des heures
                     return;
                 if (e.isPopupTrigger() && e.getComponent() instanceof JTable ) { //clic droit
-                    Maj nouv2 = null;
-                    nouv2 = new Maj(ajouter, "Ajouter ou modofier une séance", true);
-                    nouv2.setVisible(true);
+                    Maj nouv = null;
+                    nouv = new Maj(ajouter, "Ajouter ou modofier une séance", true);
+                    nouv.setVisible(true);
                 }
-                else if(SwingUtilities.isLeftMouseButton(e)){ //clic gauche
+                else if(SwingUtilities.isLeftMouseButton(e)){ //clic gauche 
                     JPopupMenu popup = createPopupMenu();
                     popup.show(e.getComponent(), e.getX(), e.getY());
                         
@@ -171,8 +123,8 @@ public class Edt extends JFrame{
             }
         });
         //ajout des objets à notre fenetre
-        this.getContentPane().add(menu,BorderLayout.NORTH);
-        this.getContentPane().add(emploi,BorderLayout.CENTER);
+            this.getContentPane().add(menu,BorderLayout.NORTH);
+            this.getContentPane().add(emploi,BorderLayout.CENTER);
         
         // pour fermer la fenetre
         addWindowListener(new WindowAdapter() {
